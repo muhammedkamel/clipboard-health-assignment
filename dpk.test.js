5b0d2149017f55e14ca44bceca07bfac7a7eae21
Mohamed Kamel <muhamed.kamel.elsayed@gmail.com>
2023-01-03 03:24:23 +0200

    finished test cases

--- dpk.test.js
+++ dpk.test.js
@@ -1,3 +1,4 @@
+const crypto = require("crypto");
 const { deterministicPartitionKey } = require("./dpk");
 
 describe("deterministicPartitionKey", () => {
@@ -6,13 +7,89 @@ describe("deterministicPartitionKey", () => {
     expect(trivialKey).toBe("0");
   });
 
-  it('returns raw partition key if length is lower than 256')
+  it('returns raw partition key if length is lower than 256', () => {
+    const partitionKey = 'Tasneem'
 
-  it('returns encrypted partition id if the partition id length is more than 256')
+    const candidateKey = deterministicPartitionKey({ partitionKey })
 
-  it('returns raw stringified partition key if the length is lower than 256')
+    expect(candidateKey).toBe(partitionKey)
+  })
 
-  it('returns encrypted partition key if the length is more than 256')
+  it('returns encrypted partition id if the partition id length is more than 256', () => {
+    const partitionKey = 'T'.repeat(257)
 
-  it('returns encrypted event if there is no partition key')
+    const candidateKey = deterministicPartitionKey({ partitionKey })
+
+    expect(candidateKey).not.toBe(partitionKey)
+
+    const encryptedPK = crypto.createHash('sha3-512').update(partitionKey).digest('hex')
+
+    expect(candidateKey).toBe(encryptedPK)
+  })
+
+  it('returns raw stringified partition key if the length is lower than 256', () => {
+    const partitionKey = { name: 'Tasneem', relationship: 'daughter' }
+
+    const candidateKey = deterministicPartitionKey({ partitionKey })
+
+    expect(candidateKey).not.toEqual(partitionKey)
+    expect(candidateKey).toBe(JSON.stringify(partitionKey))
+  })
+
+  it('returns raw stringified number partition key if the length is lower than 256', () => {
+    const partitionKey = 5
+
+    const candidateKey = deterministicPartitionKey({ partitionKey })
+
+    expect(candidateKey).not.toEqual(partitionKey)
+    expect(candidateKey).toBe(JSON.stringify(partitionKey))
+  })
+
+  it('returns encrypted partition key if the length is more than 256', () => {
+    const partitionKey = { name: 'T'.repeat(257), relationship: 'daughter' }
+
+    const candidateKey = deterministicPartitionKey({ partitionKey })
+
+    expect(candidateKey).not.toEqual(partitionKey)
+
+    const encryptedPK = crypto.createHash('sha3-512').update(JSON.stringify(partitionKey)).digest('hex')
+
+    expect(candidateKey).toBe(encryptedPK)
+  })
+
+  it('returns encrypted event if there is no partition key', () => {
+    const event = { name: 'Tasneem', relationship: 'daughter' }
+
+    const candidateKey = deterministicPartitionKey(event)
+
+    expect(candidateKey).not.toEqual(event)
+
+    const encryptedPK = crypto.createHash('sha3-512').update(JSON.stringify(event)).digest('hex')
+
+    expect(candidateKey).toBe(encryptedPK)
+  })
+
+  it('returns encrypted event if it is a string', () => {
+    const event = 'Tasneem'
+
+    const candidateKey = deterministicPartitionKey(event)
+
+    expect(candidateKey).not.toEqual(event)
+
+    const encryptedPK = crypto.createHash('sha3-512').update(JSON.stringify(event)).digest('hex')
+
+    expect(candidateKey).toBe(encryptedPK)
+  })
+
+  it('returns encrypted event if it is a number', () => {
+    const event = 5
+
+    const candidateKey = deterministicPartitionKey(event)
+
+    expect(candidateKey).not.toEqual(event)
+
+    const encryptedPK = crypto.createHash('sha3-512').update(JSON.stringify(event)).digest('hex')
+
+    expect(candidateKey).toBe(encryptedPK)
+  })
 });
